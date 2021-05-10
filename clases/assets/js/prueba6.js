97c6d4fd2d80b68a2be8593a81ffb093c93a2264
m <maritza_dy@hotmail.com>
2021-05-09 20:29:44 -0500

    cambiios

---
+++ clases/assets/js/prueba6.js
@@ -0,0 +1,11 @@
+function myFunction(){
+var x = document.getElementById("logo")
+x.style.color = "blue"
+}
+
+function formulario_test(){
+    var numero1 = document.getElementById("numero_1").value;
+    var numero2 = document.getElementById("numero_2").value;
+    var suma = parseInt(numero1) + parseInt(numero2);
+    alert(suma);;
+}
---
+++ clases/assets/js/tarea6.js
@@ -0,0 +1,10 @@
+function formulario_test(){
+   var numero1 = document.getElementById("numero1").value;
+   var numero2 = document.getElementById("numero2").value;
+   var numero3 = document.getElementById("numero3").value;
+   var suma = parseInt(numero1)+ parseInt(numero2);
+   var multiplicacion = suma *parseInt(numero3);
+    alert(suma);;
+    alert(multiplicacion);;
+
+}
